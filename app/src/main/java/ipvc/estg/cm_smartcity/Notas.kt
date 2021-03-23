3c59fc01c519b7fdf96de283e8b295b61dcad935
Renato Rocha <renato.pr99@gmail.com>
2021-03-23 20:09:20 +0000

    Activity de login efetuada

---
+++ app/src/main/java/ipvc/estg/cm_smartcity/Notas.kt
@@ -0,0 +1,12 @@
+package ipvc.estg.cm_smartcity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Notas : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notas)
+    }
+}
