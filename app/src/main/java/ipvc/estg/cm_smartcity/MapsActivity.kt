49f2affdd2f8c91e3844c04be4909b992d61b368
Renato Rocha <renato.pr99@gmail.com>
2021-05-15 21:07:42 +0100

    Notes com UPDATE, DELETE E EDIT completos

--- app/src/main/java/ipvc/estg/cm_smartcity/MapsActivity.kt
+++ app/src/main/java/ipvc/estg/cm_smartcity/MapsActivity.kt
@@ -9,7 +9,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-
+//dev
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
